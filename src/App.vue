932157afa5dd3af57c352179a7b944eb660744a1
Administrator <941144696@qq.com>
2024-03-14 17:44:15 +0800

    axios基础封装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
-//import { ElButton } from "element-plus";
 </script>
 
 <template>
